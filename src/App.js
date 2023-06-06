96109f90c2f56e16da6e2ca40cc007fd5d280e2b
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-06-06 15:32:06 +0530

    Basic routing implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation/>
+      <Switch>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
