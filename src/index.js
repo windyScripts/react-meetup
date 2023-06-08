8e134f78051eaea1db55536a077bb52f99176f73
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-06-08 15:23:48 +0530

    implemented meetups display components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BrowserRouter><App /></BrowserRouter>)
+// ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
