0a88f451c537ade843aaa9f644ef0fcc52c5d2df
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-06-08 18:39:55 +0530

    connected to firebase API using fetch to input data

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm />
-    </section>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://meetup-517b7-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "applicatoin/json" },
+      }
+    );
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
